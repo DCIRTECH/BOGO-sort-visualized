45a11374c92506a2934a41775bc2a5b231d4df11
Dominic C <domcirellitech@gmail.com>
2024-10-29 23:58:42 -0400

    figured it out

--- VisualizeAlgorithmBogo.py
+++ VisualizeAlgorithmBogo.py
@@ -10,7 +10,7 @@ def randomizer(ListOfSquare, ListOfPositions):
         ListOfPositions[c], ListOfPositions[d] = ListOfPositions[d], ListOfPositions[c]
     return ListOfSquare, ListOfPositions
 
-
+#done
 class SquareToCircle(Scene):
     def construct(self):
         squareBlue = squareBlue2 = squareBlue3 = squareBlue4 = squareBlue5 = Square().scale(.5)
